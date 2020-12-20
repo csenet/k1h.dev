bbb0341777f9885fffd45103e2aad03187ef920f
Koichi Hirachi <kouichi.hirachi@gmail.com>
2020-12-20 19:03:37 +0900

    Change unvisible.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -6,12 +6,11 @@
     toggleable="lg"
   >
     <b-container>
-      <b-navbar-brand to="/">Koichi Hirachi</b-navbar-brand>
+      <b-navbar-brand to="/">KOU</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-item to="/works">Works</b-nav-item>
-          <b-nav-item to="/skills">Skills</b-nav-item>
           <b-nav-item to="/about">About</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
---
+++ layouts/simple.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+<script>
+import Footer from "~/components/Footer.vue";
+export default {
+  components: {
+    Footer,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Ubuntu, "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN",
+    "Hiragino Sans", Meiryo, sans-serif;
+}
+.subtitle {
+  display: block;
+  padding-left: 8px;
+  border-left: 3px solid #1a1a1a;
+  padding-top: 5px;
+}
+</style>
