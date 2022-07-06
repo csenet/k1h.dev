47abf8947a5c423166d17663935ad83e483145f5
VOID <kouichi.hirachi@gmail.com>
2022-07-06 15:44:12 +0900

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
      ** See https://nuxtjs.org/api/configuration-head
      */
     head: {
-        title: process.env.npm_package_name || '',
+        title: process.env.npm_package_name || 'k1h Portfolio',
         meta: [{
                 charset: 'utf-8'
             },
